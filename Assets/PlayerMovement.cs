09994909a0d220dd56f2e72bd8db2efd8911df45
Erik Uggeldahl <erikuggeldahl@gmail.com>
2013-12-13 22:02:54 -0500

    Added a testbest level.
    Preliminary pass for player movement.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	float maxSpeed = 10f;
+	float moveSpeed = 10f;
+	
+	void Start()
+	{
+	
+	}
+	
+	void FixedUpdate()
+	{
+		Vector3 movement = Vector3.zero;
+		movement.z += Input.GetAxis("Vertical");
+		movement.x += Input.GetAxis("Horizontal");
+		movement.Normalize();
+		
+		movement *= moveSpeed * Time.fixedDeltaTime;
+		if (rigidbody.velocity.magnitude + movement.magnitude > maxSpeed)
+			return;
+		
+		rigidbody.AddForce(movement, ForceMode.VelocityChange);
+	}
+	
+	void Run()
+	{
+		Vector3 movement = Vector3.zero;
+		movement.z += Input.GetAxis("Vertical");
+		movement.x += Input.GetAxis("Horizontal");
+		movement.Normalize();
+		
+		movement *= moveSpeed * Time.fixedDeltaTime;
+		if (rigidbody.velocity.magnitude + movement.magnitude > maxSpeed)
+			return;
+		
+		rigidbody.AddForce(movement, ForceMode.VelocityChange);
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label(rigidbody.velocity.magnitude.ToString());
+	}
+}
